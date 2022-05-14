77377f88d1a0879871824b997ca84306d6e3b5b2
Steven Silvester <steven.silvester@ieee.org>
2022-05-14 06:39:58 -0500

    path cleanup

--- tests/test_npm_builder.py
+++ tests/test_npm_builder.py
@@ -7,71 +7,76 @@ import pytest
 from hatch_jupyter_builder import npm_builder
 
 
-def test_npm_builder(mocker):
+@pytest.fixture
+def repo(tmp_path):
+    os.makedirs(os.path.join(tmp_path, ".git"))
+    return tmp_path
+
+
+def test_npm_builder(mocker, repo):
     which = mocker.patch("hatch_jupyter_builder.utils.which")
     run = mocker.patch("hatch_jupyter_builder.utils.run")
     which.return_value = "foo"
-    npm_builder("wheel", "standard", force=True)
-    cwd = str(Path.cwd())
+    npm_builder("wheel", "standard", path=repo)
     run.assert_has_calls(
-        [call(["foo", "install"], cwd=cwd), call(["foo", "run", "build"], cwd=cwd)]
+        [call(["foo", "install"], cwd=str(repo)), call(["foo", "run", "build"], cwd=str(repo))]
     )
 
 
-def test_npm_build_skip(mocker):
+def test_npm_build_skip(mocker, repo):
     which = mocker.patch("hatch_jupyter_builder.utils.which")
     run = mocker.patch("hatch_jupyter_builder.utils.run")
     os.environ["HATCH_JUPYTER_BUILDER_SKIP_NPM"] = "1"
     which.return_value = "foo"
-    npm_builder("wheel", "standard", force=True)
+    npm_builder("wheel", "standard", path=repo)
     run.assert_not_called()
     del os.environ["HATCH_JUPYTER_BUILDER_SKIP_NPM"]
 
 
-def test_npm_builder_yarn(tmp_path, mocker):
+def test_npm_builder_yarn(mocker, repo):
     which = mocker.patch("hatch_jupyter_builder.utils.which")
     run = mocker.patch("hatch_jupyter_builder.utils.run")
-    tmp_path.joinpath("yarn.lock").write_text("hello")
+    repo.joinpath("yarn.lock").write_text("hello")
     which.return_value = "foo"
-    npm_builder("wheel", "standard", path=tmp_path, force=True)
+    npm_builder("wheel", "standard", path=repo)
     run.assert_has_calls(
         [
-            call(["foo", "install"], cwd=str(tmp_path)),
-            call(["foo", "run", "build"], cwd=str(tmp_path)),
+            call(["foo", "install"], cwd=str(repo)),
+            call(["foo", "run", "build"], cwd=str(repo)),
         ]
     )
 
 
-def test_npm_builder_missing_yarn(tmp_path, mocker):
+def test_npm_builder_missing_yarn(mocker, repo):
     which = mocker.patch("hatch_jupyter_builder.utils.which")
     run = mocker.patch("hatch_jupyter_builder.utils.run")
-    tmp_path.joinpath("yarn.lock").write_text("hello")
+    repo.joinpath("yarn.lock").write_text("hello")
     which.side_effect = ["", "foo"]
-    npm_builder("wheel", "standard", path=tmp_path, force=True)
+    npm_builder("wheel", "standard", path=repo)
     run.assert_has_calls(
         [
-            call(["foo", "install"], cwd=str(tmp_path)),
-            call(["foo", "run", "build"], cwd=str(tmp_path)),
+            call(["foo", "install"], cwd=str(repo)),
+            call(["foo", "run", "build"], cwd=str(repo)),
         ]
     )
 
 
-def test_npm_builder_not_stale(tmp_path, mocker):
+def test_npm_builder_not_stale(mocker, repo):
     which = mocker.patch("hatch_jupyter_builder.utils.which")
     run = mocker.patch("hatch_jupyter_builder.utils.run")
     is_stale = mocker.patch("hatch_jupyter_builder.utils.is_stale")
     is_stale.return_value = False
     which.return_value = "foo"
-    npm_builder("wheel", "standard", build_dir=tmp_path, source_dir=tmp_path, force=True)
+    npm_builder("wheel", "standard", path=repo, build_dir=repo, source_dir=repo)
     run.assert_not_called()
 
 
-def test_npm_builder_no_npm(mocker):
+def test_npm_builder_no_npm(mocker, repo):
     which = mocker.patch("hatch_jupyter_builder.utils.which")
     run = mocker.patch("hatch_jupyter_builder.utils.run")
     is_stale = mocker.patch("hatch_jupyter_builder.utils.is_stale")
     is_stale.return_value = False
     which.return_value = ""
     with pytest.raises(ValueError):
-        npm_builder("wheel", "standard", force=True)
+        npm_builder("wheel", "standard", path=repo)
     run.assert_not_called()
