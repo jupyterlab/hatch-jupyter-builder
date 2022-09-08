3ee61230601f641a052f5862a2a2bfa06a614eb0
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-09-08 22:17:29 +0000

    Publish 0.6.0
    SHA256 hashes:
    
    hatch_jupyter_builder-0.6.0-py3-none-any.whl: baf3384420f7e869a0af4c27b5aa4627f508f3357d31dcc889dd6a38fd7e4fbc
    
    hatch_jupyter_builder-0.6.0.tar.gz: 7d3c96a666d254237b88d780f15cb130e07e9dc8215306ffd4c3d0a3050a7748

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Project Jupyter"
 author = "Project Jupyter"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.3"
+release = "0.6.0"
 
 
 # -- General configuration ---------------------------------------------------
