067fccb0c52de69d71a755080ea362528110bc40
Steven Silvester <steven.silvester@ieee.org>
2023-09-05 04:57:05 -0500

    Fix migration test (#124)
    cleanup

--- tests/data/npm_builder/myextension/src/index.ts
+++ tests/data/npm_builder/myextension/src/index.ts
@@ -3,31 +3,16 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
-
 /**
  * Initialization data for the myextension extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'myextension:plugin',
   autoStart: true,
-  optional: [ISettingRegistry],
   activate: (
-    app: JupyterFrontEnd,
-    settingRegistry: ISettingRegistry | null
+    app: JupyterFrontEnd
   ) => {
     console.log('JupyterLab extension myextension is activated!');
-
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('myextension settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for myextension.', reason);
-        });
-    }
   }
 };
 
